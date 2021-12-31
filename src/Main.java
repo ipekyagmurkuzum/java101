d4fe66f992a067c414e7437e0244521586d5c6f2
ipekyagmur <yagmurkuzuum@gmail.com>
2022-01-01 01:33:39 +0300

    User login added.

--- src/Main.java
+++ src/Main.java
@@ -234,6 +234,7 @@ public class Main {
 
         //BASİT HESAP MAKİNESİ----------------------------------------------
 
+        /*
         double n1, n2;
 
         Scanner input = new Scanner(System.in);
@@ -266,6 +267,41 @@ public class Main {
                 break;
             default:
                 System.out.print("Hatalı bir sayı girdiniz. Lütfen tekrar deneyin.");
+        }
+
+         */
+
+
+        //KULLANICI GİRİŞİ----------------------------------------------
+
+        String kullaniciAdi = "patika", sifre = "java101", kullaniciAdi_input, sifre_input;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kullanıcı adınızı giriniz: ");
+        kullaniciAdi_input = input.nextLine();
+        System.out.print("Şifrenizi giriniz: ");
+        sifre_input = input.nextLine();
+
+        if (kullaniciAdi_input.equals(kullaniciAdi) && sifre_input.equals(sifre)) {
+            System.out.print("Giriş başarılı!");
+        } else if (!(sifre_input.equals("java101"))) {
+            System.out.print("Yanlış şifre girdiniz. Şifrenizi sıfırlamak için lütfen 1'e basın.");
+            int secenek = input.nextInt();
+            input.nextLine();
+            if (secenek == 1) {
+                System.out.print("Lütfen yeni şifrenizi girin: ");
+                String yeni_sifre = input.nextLine();
+                if (yeni_sifre.equals(sifre) || yeni_sifre.equals(sifre_input)) {
+                    System.out.print("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+                } else {
+                    sifre = yeni_sifre;
+                    System.out.print("Şifre oluşturuldu.");
+                }
+            } else {
+                System.out.print("İşlem iptal edildi.");
+            }
+        } else {
+            System.out.print("Lütfen bilgilerinizi kontrol ediniz.");
         }
     }
 }
