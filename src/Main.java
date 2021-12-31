53bd9dadbde2fcc399f6d9cc3664cf4d91119d2c
ipekyagmur <yagmurkuzuum@gmail.com>
2021-12-31 21:43:43 +0300

    Circular calculations added.

--- src/Main.java
+++ src/Main.java
@@ -107,7 +107,7 @@ public class Main {
 
         //TAKSİMETRE----------------------------------------------
 
-
+        /*
         double acilisUcreti = 10,toplam,km;
 
         Scanner input = new Scanner(System.in);
@@ -121,5 +121,28 @@ public class Main {
         }
 
         System.out.println("Toplam ödenecek tutar: " + toplam + " TL'dir.");
+
+        */
+
+
+        //DAİRE ÇEVRESİ VE ALANI HESAPLAMA----------------------------------------------
+
+        double yaricap,cevre,alan, pi = 3.14, alfa, dilim_alan;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yarıçap değeri giriniz: ");
+        yaricap =input.nextDouble();
+
+        cevre = 2 * pi * yaricap;
+        alan = pi* Math.pow(yaricap,2);
+        //System.out.print("Çevre: "+ cevre + " Alan: " + alan);
+
+        Scanner a_input = new Scanner(System.in);
+        System.out.print("Açı değerini giriniz");
+        alfa = a_input.nextDouble();
+
+        dilim_alan = alan*alfa/360;
+        System.out.println("Daire diliminin alanı: " + dilim_alan);
+
     }
 }
