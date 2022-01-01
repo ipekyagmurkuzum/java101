852144f06e5aff94328ce30ab0d57b4c9714865c
ipekyagmur <yagmurkuzuum@gmail.com>
2022-01-01 19:47:57 +0300

    Chinese zodiac calculator added.

--- src/Main.java
+++ src/Main.java
@@ -575,6 +575,7 @@ public class Main {
 
         //UÇAK BİLETİ FİYATI HESAPLAMA----------------------------------------------
 
+        /*
 
         Scanner input = new Scanner(System.in);
         System.out.print("Mesafeyi km türünden giriniz: ");
@@ -607,6 +608,60 @@ public class Main {
         } else {
             System.out.print("Hatalı giriş yaptınız.");
         }
+
+         */
+
+
+        //ÇİN ZODYAĞI----------------------------------------------
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen doğum yılınızı giriniz: ");
+        int yil = input.nextInt();
+
+        int zodyak = yil % 12;
+
+        switch (zodyak) {
+            case 0:
+                System.out.print("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.print("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.print("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.print("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.print("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.print("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.print("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.print("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.print("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.print("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.print("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.print("Çin Zodyağı Burcunuz : Koyun");
+                break;
+            default:
+                System.out.print("Hatalı giriş yaptınız.");
+        }
+
     }
 }
+
 
