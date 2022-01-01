1283d889bc370869a53840e27fb3315f799ed5ed
ipekyagmur <yagmurkuzuum@gmail.com>
2022-01-01 14:49:28 +0300

    Class pass status added.

--- src/Main.java
+++ src/Main.java
@@ -274,6 +274,8 @@ public class Main {
 
         //KULLANICI GİRİŞİ----------------------------------------------
 
+
+        /*
         String kullaniciAdi = "patika", sifre = "java101", kullaniciAdi_input, sifre_input;
 
         Scanner input = new Scanner(System.in);
@@ -302,6 +304,71 @@ public class Main {
             }
         } else {
             System.out.print("Lütfen bilgilerinizi kontrol ediniz.");
+        }
+
+         */
+
+
+        //SINIF GEÇME DURUMU----------------------------------------------
+
+        int mat, fizik, kimya, biyo, muzik, sayac = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik notunuzu girin: ");
+        mat = input.nextInt();
+        boolean kosul1 = (mat <= 100) && (mat > 0);
+        if (kosul1 == true) {
+            sayac = sayac + 1;
+        } else {
+            mat = 0;
+        }
+
+        System.out.print("Fizik notunuzu girin: ");
+        fizik = input.nextInt();
+        boolean kosul2 = (fizik <= 100) && (fizik > 0);
+        if (kosul2 == true) {
+            sayac = sayac + 1;
+        } else {
+            fizik = 0;
+        }
+
+        System.out.print("Kimya notunuzunu girin: ");
+        kimya = input.nextInt();
+        boolean kosul3 = (kimya <= 100) && (kimya > 0);
+        if (kosul3 == true) {
+            sayac = sayac + 1;
+        } else {
+            kimya = 0;
+        }
+
+        System.out.print("Biyoloji notunuzu girin: ");
+        biyo = input.nextInt();
+        boolean kosul4 = (biyo <= 100) && (biyo > 0);
+        if (kosul4 == true) {
+            sayac = sayac + 1;
+        } else {
+            biyo = 0;
+        }
+
+        System.out.print("Müzik notunuzu girin: ");
+        muzik = input.nextInt();
+        boolean kosul5 = (muzik <= 100) && (muzik > 0);
+        if (kosul5 == true) {
+            sayac = sayac + 1;
+        } else {
+            muzik = 0;
+        }
+
+        if (sayac > 0) {
+            double ortalama = (mat + fizik + kimya + biyo + muzik) / sayac;
+            if (ortalama <= 55) {
+                System.out.print("Ortalamanız " + ortalama + ".\nSınıfta kaldınız. ");
+            } else {
+                System.out.print("Ortalamanız " + ortalama + ".\nTebrikler! Sınıfı geçtiniz. ");
+            }
+        } else {
+            System.out.print("Lütfen 0-100 arası not giriniz. ");
         }
     }
 }
