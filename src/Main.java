96230b06f6bf53f4064a5f6104c505a684b9fa78
ipekyagmur <yagmurkuzuum@gmail.com>
2022-01-01 19:36:52 +0300

    Flight ticket

--- src/Main.java
+++ src/Main.java
@@ -579,23 +579,23 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.print("Mesafeyi km türünden giriniz: ");
         int km = input.nextInt();
-        boolean kosul1 = (km>0);
+        boolean kosul1 = (km > 0);
 
         System.out.print("Yaşınızı giriniz: ");
         int yas = input.nextInt();
-        boolean kosul2 = (yas>0);
+        boolean kosul2 = (yas > 0);
 
         System.out.print("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): ");
         int yolculukTipi = input.nextInt();
-        boolean kosul3 = ((yolculukTipi==1) || (yolculukTipi==2));
+        boolean kosul3 = ((yolculukTipi == 1) || (yolculukTipi == 2));
 
-        boolean kosul = (kosul1&&kosul2&&kosul3);
+        boolean kosul = (kosul1 && kosul2 && kosul3);
 
         if (kosul) {
             double biletFiyati = 0.1 * km;
             if (yas < 12) {
                 biletFiyati *= 0.5;
-            } else if ((yas >=12) && (yas <=24)) {
+            } else if ((yas >= 12) && (yas <= 24)) {
                 biletFiyati *= 0.9;
             } else if (yas > 65) {
                 biletFiyati *= 0.7;
