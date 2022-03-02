76bd068bbd4ecbaa6966869d0881d3b0f7f53245
ipekyagmur <yagmurkuzuum@gmail.com>
2022-03-02 21:24:09 +0300

    practice

---
+++ src/BHarfi.java
@@ -0,0 +1,24 @@
+public class BHarfi {
+    public static void main(String[] args) {
+
+        String[][] harf = new String[7][4];
+
+        for (int i = 0; i < harf.length; i++) {
+            for (int j = 0; j < harf[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) {
+                    harf[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    harf[i][j] = " * ";
+                } else {
+                    harf[i][j] = "   ";
+                }
+            }
+        }
+        for(String[] row : harf) {
+            for (String col :row ) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
