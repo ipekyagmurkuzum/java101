5a56496a830c230367793dcf57b215f3a0dc51f0
Yagmur <yagmurkuzuum@gmail.com>
2022-04-02 18:54:28 +0300

    final project

---
+++ src/MayinTarlasi/Main.java
@@ -0,0 +1,8 @@
+package MayinTarlasi;
+
+public class Main {
+    public static void main(String[] args) {
+        MineSweeper mine = new MineSweeper(5,10);
+        mine.run();
+    }
+}
---
+++ src/MayinTarlasi/MineSweeper.java
@@ -0,0 +1,73 @@
+package MayinTarlasi;
+
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MineSweeper {
+    int mineNumber, selectedRow, selectedCol;
+    Scanner scan = new Scanner(System.in);
+    Random random = new Random();
+    int occupiedSpots = 0, nearMines = 0;
+    boolean isGameOver = false;
+
+    public MineSweeper(int rowNumber, int colNumber) {
+        String[][] matrix = new String[rowNumber][colNumber];
+        mineNumber = (rowNumber * colNumber) / 4;
+
+        while (occupiedSpots < mineNumber) {
+            int x = random.nextInt(rowNumber);
+            int y = random.nextInt(colNumber);
+            matrix[x][y] = "*";
+            occupiedSpots++;
+        }
+
+        for (int i = 0; i < rowNumber; i++) {
+            for (int j = 0; j < colNumber; j++) {
+                if (matrix[i][j] == null) {
+                    matrix[i][j] = "-";
+                }
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        while (!isGameOver) {
+            System.out.println("Satır sayısı giriniz: ");
+            selectedRow = scan.nextInt() - 1;
+            System.out.println("Sütun sayısı giriniz: ");
+            selectedCol = scan.nextInt() - 1;
+            System.out.println("======================");
+
+            if (selectedRow < 0 || selectedRow > rowNumber) {
+                System.out.println("Hatalı sayı girişi");
+            } else if (selectedCol < 0 || selectedCol > colNumber) {
+                System.out.println("Hatalı sayı girişi");
+            } else if (Objects.equals(matrix[selectedRow][selectedCol], "*")) {
+                isGameOver = true;
+                System.out.println("Game Over!");
+            } else if (Objects.equals(matrix[selectedRow][selectedCol], "-")) {
+                for (int i = selectedRow - 1; i <= selectedRow + 1; i++) {
+                    for (int j = selectedCol - 1; j <= selectedCol + 1; j++) {
+                        if ((i < rowNumber && i > 0) && (j < colNumber && j > 0)) {
+                            if (Objects.equals(matrix[i][j], "*")) {
+                                nearMines++;
+                            }
+                        }
+                    }
+                }
+                matrix[selectedRow][selectedCol] = String.valueOf(nearMines);
+                for (String[] row : matrix) {
+                    for (String col : row) {
+                        System.out.print(col);
+                    }
+                    System.out.println();
+                }
+            } else {
+                System.out.println("Bu satır zaten açıldı.");
+            }
+        }
+    }
+
+    public static void run() {
+    }
+}
